52f5007eeebeca39aa70172c1152001a91b040ae
epierrej <halodoctor3000@yahoo.com>
2017-10-05 17:31:45 -0500

    new JFrame code

---
+++ JFrame Java/src/EventPage.java
@@ -0,0 +1,46 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class EventPage extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EventPage frame = new EventPage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public EventPage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblEventbrite = new JLabel("EventBrite");
+		lblEventbrite.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblEventbrite.setBounds(151, 13, 102, 16);
+		contentPane.add(lblEventbrite);
+	}
+}
