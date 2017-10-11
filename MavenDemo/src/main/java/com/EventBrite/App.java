85125ec0345233ec52ed493b8d967eec87bc813d
epierrej <halodoctor3000@yahoo.com>
2017-10-11 11:09:21 -0500

    MavenDemo with Pom.xml

---
+++ MavenDemo/src/main/java/com/EventBrite/App.java
@@ -0,0 +1,7 @@
+package com.EventBrite;
+
+public class App {
+	public String Sample() {
+		return "Hello world!";
+	}
+}
---
+++ MavenDemo/src/test/java/com/EventBrite/AppTest.java
@@ -0,0 +1,14 @@
+package com.EventBrite;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		System.out.println("Hello World!");
+		//fail("Not yet implemented");
+	}
+
+}
